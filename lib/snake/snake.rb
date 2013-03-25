81d9fbc11d6eb14640ac007092435f1fb7b6af6d
Daniel Grieve <git@dgrieve.com>
2013-03-25 20:59:03 +0000

    the tail moves!

--- lib/snake/snake.rb
+++ lib/snake/snake.rb
@@ -3,17 +3,36 @@ module Snake
     extend Forwardable
     def_delegators :@game, :draw_quad
 
+    attr_reader :tail
+
+    START_LOCATION = [100, 100]
+    START_DIRECTION = [10, 0]
+    START_LENGTH = 3
+    SPEED = 500
+
     def initialize(game)
       @game = game
       @last_moved = 0
 
-      @head = Head.new(self, 100, 100)
+      @head = Head.new(self, START_LOCATION[0], START_LOCATION[1])
+      @head.direction = START_DIRECTION
+
+      @tail = []
+
+      (1..START_LENGTH).each do |num|
+        tail = Tail.new(self, START_LOCATION[0] - num*@head.width, START_LOCATION[1])
+        num.times { tail.queue << START_DIRECTION }
+        @tail << tail
+      end
     end
 
     def update
       move_time = Gosu::milliseconds
-      if @last_moved + 500 < move_time
+      if @last_moved + SPEED < move_time
         @head.move
+        @tail.each do |tail|
+          tail.move
+        end
 
         @last_moved = move_time
       end
@@ -21,17 +40,20 @@ module Snake
 
     def draw
       @head.draw
+      @tail.each do |tail|
+        tail.draw
+      end
     end
 
     def button_down(id)
       if id == Gosu::KbUp
-        @head.move_up
+        @head.direction = [0, -10]
       elsif id == Gosu::KbDown
-        @head.move_down
+        @head.direction = [0, 10]
       elsif id == Gosu::KbRight
-        @head.move_right
+        @head.direction = [10, 0]
       elsif id == Gosu::KbLeft
-        @head.move_left
+        @head.direction = [-10, 0]
       end
     end
   end
@@ -40,7 +62,7 @@ module Snake
     extend Forwardable
     def_delegators :@snake, :draw_quad
 
-    attr_accessor :x, :y, :width, :height, :direction
+    attr_accessor :x, :y, :width, :height
 
     def initialize(snake, x, y)
       @snake = snake
@@ -48,8 +70,6 @@ module Snake
       @y = y
       @width = 10
       @height = 10
-
-      @direction = [10, 0]
     end
 
     def draw
@@ -63,25 +83,35 @@ module Snake
   end
 
   class Head < Segment
+    attr_accessor :direction
+
+    def initialize(snake, x, y)
+      super(snake, x, y)
+    end
+
     def move
       @x += @direction[0]
       @y += @direction[1]
+
+      @snake.tail.each do |tail|
+        tail.queue << @direction
+      end
+    end
+  end
+
+  class Tail < Segment
+    attr_accessor :queue
+
+    def initialize(snake, x, y)
+      super(snake, x, y)
+      @queue = []
     end
 
-    def move_down
-      @direction = [0, 10]
-    end
+    def move
+      direction = @queue.shift
 
-    def move_left
-      @direction = [-10, 0]
-    end
-
-    def move_right
-      @direction = [10, 0]
-    end
-
-    def move_up
-      @direction = [0, -10]
+      @x += direction[0]
+      @y += direction[1]
     end
   end
 end
