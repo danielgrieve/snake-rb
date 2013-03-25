5877c1a9f9341ef73c594f44ff5d6f62769e247c
Daniel Grieve <git@dgrieve.com>
2013-03-25 20:21:00 +0000

    the head moves

--- lib/snake/snake.rb
+++ lib/snake/snake.rb
@@ -1,66 +1,87 @@
 module Snake
   class Snake
     extend Forwardable
-
     def_delegators :@game, :draw_quad
 
     def initialize(game)
       @game = game
-
-      @blocks = []
-      @blocks << Block.new(50, 50)
-      @blocks << Block.new(60, 50)
-      @blocks << Block.new(70, 50)
-
       @last_moved = 0
+
+      @head = Head.new(self, 100, 100)
     end
 
     def update
       move_time = Gosu::milliseconds
       if @last_moved + 500 < move_time
-        @blocks.each do |block|
-          block.move
-        end
+        @head.move
+
         @last_moved = move_time
       end
     end
 
     def draw
-      @blocks.each do |block|
-        block.draw(self)
-      end
+      @head.draw
     end
 
     def button_down(id)
       if id == Gosu::KbUp
+        @head.move_up
       elsif id == Gosu::KbDown
+        @head.move_down
       elsif id == Gosu::KbRight
+        @head.move_right
       elsif id == Gosu::KbLeft
+        @head.move_left
       end
     end
   end
 
-  class Block
-    attr_accessor :x, :y, :width, :height
+  class Segment
+    extend Forwardable
+    def_delegators :@snake, :draw_quad
 
-    def initialize(x, y)
-      self.x = x
-      self.y = y
-      self.width = 10
-      self.height = 10
+    attr_accessor :x, :y, :width, :height, :direction
+
+    def initialize(snake, x, y)
+      @snake = snake
+      @x = x
+      @y = y
+      @width = 10
+      @height = 10
+
+      @direction = [10, 0]
     end
 
-    def draw(game)
-      game.draw_quad(
+    def draw
+      draw_quad(
         x,          y,           Gosu::Color::BLACK,
         x + width,  y,           Gosu::Color::BLACK,
         x,          y + height,  Gosu::Color::BLACK,
         x + width,  y + height,  Gosu::Color::BLACK
       )
     end
+  end
 
+  class Head < Segment
     def move
-      self.x += width
+      @x += @direction[0]
+      @y += @direction[1]
+    end
+
+    def move_down
+      @direction = [0, 10]
+    end
+
+    def move_left
+      @direction = [-10, 0]
+    end
+
+    def move_right
+      @direction = [10, 0]
+    end
+
+    def move_up
+      @direction = [0, -10]
     end
   end
 end
